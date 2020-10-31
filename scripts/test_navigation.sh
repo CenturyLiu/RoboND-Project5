5089796e6a0a3f81af225a7986cde86265a56f88
Liu Shiji <shijiliu@umich.edu>
2020-10-31 12:04:21 -0400

    feat: update file link for the shell scripts

--- scripts/test_navigation.sh
+++ scripts/test_navigation.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:="/home/centuryliu/robotic_self_learning_ws/src/my_robot/worlds/classroom.world"" &
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:="/home/centuryliu/robotic_self_learning_ws/src/my_robot/worlds/open_classroom.world"" &
 sleep 5
-xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:="/home/centuryliu/robotic_self_learning_ws/src/my_robot/maps/classroom_map.yaml"" &
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:="/home/centuryliu/robotic_self_learning_ws/src/my_robot/maps/open_classroom_map.yaml"" &
 sleep 5
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" 
--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:="/home/centuryliu/robotic_self_learning_ws/src/my_robot/worlds/classroom.world"" &
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:="/home/centuryliu/robotic_self_learning_ws/src/my_robot/worlds/open_classroom.world"" &
 sleep 5
 xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch" &
 sleep 5
